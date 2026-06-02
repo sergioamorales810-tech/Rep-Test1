f7257e888b83e0714387200dac49b0aecd82dcb5
sergioamorales810-tech <sergioamorales810@hotmail.com>
2026-06-02 15:22:48 -0600

    Add files via upload

---
+++ CondicionSimpleIF.java
@@ -0,0 +1,22 @@
+
+package condicionsimpleif;
+
+public class CondicionSimpleIF {
+
+    
+    public static void main(String[] args) {
+        //DEMOSTRAR MEDIANTE PANTALLA SI UN NUMERO ES MAYOR QUE OTRO
+
+        int num1 = 15;
+        int num2 = 10;
+
+        if (num2 > num1) {
+            //SI SE CUMPLE LA PRIMER CONDICION
+            System.out.println("EL NUM2 ES MAYOR AL NUM1");
+        } else {
+            //SI NO SE CUMPLE LA PRIMER CONDICION
+            System.out.println("EL NUM1 ES MAYOR AL NUM2");
+        }
+    }
+
+}
